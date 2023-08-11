ac930124cb4bf0c94f284ce4fd46db1c04bbb1d3
Karen Du <karenkaydu@gmail.com>
2023-08-10 18:31:14 -0700

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
 
 const skaters = {
     'tony hawk':{
@@ -37,6 +40,6 @@ app.get('/api/:name', (request, response)=>{
 
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is now running on port ${PORT}! Betta go catch it!`)
 })  
